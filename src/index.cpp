e163c885dc29bfb8b8115c93a8a657053efeab0a
izzuishere <joshua.j.cassell@gmail.com>
2020-08-28 17:21:19 +0100

    small changes

--- src/index.cpp
+++ src/index.cpp
@@ -15,35 +15,91 @@ class Complex {
 		}
 };
 
-bool stays_bounded() {
+int stays_bounded(double x, double i) {
 
 	Complex c;
-	cout << "Enter Real Part, dummy" << endl;
-	cin >> c.real_part;
-	cout << "Enter Complex Part, dummy" << endl;
-	cin >> c.complex_part;
+
+	c.real_part = 1;
+	c.complex_part = 0;
+	//cout << c.magnitude() << endl;
+
+	c.real_part = x;
+	c.complex_part = i;
 
 	Complex z;
 	z.real_part = 0;
 	z.complex_part = 0;
 
-	double i, x;
+	int count = 0;
 
-	double count = 0;
 	while (count < 100) {
 		z.squared(&x,&i);
+		//cout << "x = " << x << " i = " << i << endl;
 		z.real_part = x + c.real_part;
 		z.complex_part = i + c.complex_part;
+		//cout << count << ". z = " << z.real_part << " + " << i << "i" << endl;
+		//cout << "|z| = " << z.magnitude() << endl;
 		count++;
-		if (z.magnitude() > 2) {
-			cout << z.magnitude() << endl;
-			return 0;
+		if (z.magnitude() >= 2 || z.magnitude() <= -2) {
+			//cout << z.magnitude() << endl;
+			return count;
 		}
 	}
-	cout << z.magnitude() << endl;
-	return 1;
+	//cout << z.magnitude() << endl;
+	return 0;
 }
 
 int main() {
-	cout << stays_bounded() << endl;
+	unsigned char pix[2000000];
+	int res_x 	= 1000;
+	int res_y 	= 1000;
+	double zoom 	= 3;
+	double pos_x 	= 0.26109119081845;
+	double pos_y 	= 0.00205;
+	for (int x = -res_x/2; x <= res_x/2; x++) {
+		for (int i = -res_y/2; i <= res_y/2; i++){
+			//cout << x << " + " << i << "i" << endl;
+			double d = x;
+			double dx = d/(res_x*pow(10, zoom)) + pos_x;
+			d = i;
+			double di = d/(res_x*pow(10, zoom)) + pos_y;
+			//cout << stays_bounded(dx, di) << endl;
+			int in = stays_bounded(dx, di);
+			if (in > 0) {
+				//cout << "x = " << dx << ", i = " << di << " Position = " << x+res_x/2 + (i+res_y/2)*res_x << " -- " << (in%16)*16 << endl;
+
+				pix[x+res_x/2 + (i+res_y/2)*res_x]	= ((in%16)*16);
+				//pix[x+res_x/2 + (i+res_y/2)*res_x +1]	= (255);
+				//pix[x+res_x/2 + (i+res_y/2)*res_x +2]	= (255);
+			} else {
+				//cout << "x = " << dx << ", i = " << di << " Position = " << x+res_x/2 + (i+res_y/2)*res_x << endl;
+
+				pix[x+res_x/2 + (i+res_y/2)*res_x]	= (0);
+				//pix[x+res_x/2 + (i+res_y/2)*res_x +1]	= (0);
+				//pix[x+res_x/2 + (i+res_y/2)*res_x +2]	= (0);
+			}
+		}
+	}
+
+	FILE *output;
+	int x,y,pixel,height=res_x,width=res_y;
+   	//unsigned char pix[]={200,200,200, 100,100,100, 0,0,0, 255,0,0, 0,255,0, 0,0,255};
+	//pix[1] = (100);
+	output=fopen("image.pgm","wb");
+	if(output==NULL){
+		perror("ERROR: Cannot open output file");
+		exit(EXIT_FAILURE);
+	}
+
+
+	fprintf(output,"P5\n");
+	fprintf(output,"%d %d\n",width,height);
+	fprintf(output,"255\n");
+
+
+	fwrite(pix,1,16000000,output);
+
+
+	fclose(output);
+	//cout << endl << stays_bounded(-1,0);
 }
